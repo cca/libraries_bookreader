69dee843a0b02e9549310f8392c111dded7e28ea
phette23 <phette23@gmail.com>
2018-09-13 13:00:28 -0700

    better comments

--- js/app.js
+++ js/app.js
@@ -1,48 +1,50 @@
+// vaultItem is in global scope from parse-qs.js
 var vault_url = vaultItem.root + 'items/' + vaultItem.id + '/' + vaultItem.version + '/'
-// Override page dimension defaults using first page as model
-// page we choose here is arbitrary, we assume all have same aspect ratio
-// must put below getPageURI if we want to use that method
-var pageOne = vaultItem.root + 'file/' + vaultItem.id + '/' + vaultItem.version + '/' + vaultItem.filenames + '1.JPG';
-var img = new Image();
-// when image loads, redefine getPage dimension methods
+var img = new Image()
+
+// we put all of the Bookreader definition inside this img.onload function
+// so that we can use the dimensions of the first page to define dynamic
+// getPageHeight & getPageWidth methods
 img.onload = function(event) {
-    // see HTMLImageElement object: http://www.w3.org/TR/html5/embedded-content-0.html#htmlimageelement
+    // see HTMLImageElement object:
+    // http://www.w3.org/TR/html5/embedded-content-0.html#htmlimageelement
     // these two props *should* be equal for an image not yet insert into DOM
     // but just in case we prefer natural* props
-    var w = this.naturalWidth || this.width;
-    var h = this.naturalHeight || this.height;
+    var w = this.naturalWidth || this.width
+    var h = this.naturalHeight || this.height
 
     var options = {
+        // these are all defaults anyways
         ui: 'full',
         el: '#BookReader',
         enableMobileNav: true,
+
         mobileNavTitle: vaultItem.title,
+        // info dialog details
         metadata: [
             { label: 'Title', value: vaultItem.title.split(' / ')[0] },
             { label: 'Author', value: vaultItem.title.split(' / ')[1].replace(/\s*by\s+/i, '').replace(/\.$/, '') }
         ],
+
         thumbnail: vault_url.replace('/items/', '/thumbs/') + '?gallery=preview',
         bookUrlMoreInfo: vault_url,
 
-        // Total number of leafs
+        // Total number of leaves
         getNumLeafs: function () { return vaultItem.pages },
         // Book title & URL used for the book title link
         bookTitle: vaultItem.title,
         bookUrl: vault_url,
 
-        // We load the images from archive.org -- you can modify this function to retrieve images
-        // using a different URL structure
-        getPageURI: function(index, reduce, rotate) {
-            // reduce and rotate are ignored in this simple implementation, but we
-            // could e.g. look at reduce and load images from a different directory
-            // or pass the information to an image server
+        // retrieve images from VAULT using their index
+        getPageURI: function(index) {
             var url = vaultItem.root + 'file/' + vaultItem.id + '/' + vaultItem.version + '/' + vaultItem.filenames + (index + 1) + '.JPG';
             return url;
         },
+        // the whole reason we're inside an img.onload handler
         getPageHeight: function() { return h },
         getPageWidth: function() { return w },
 
-        // Return which side, left or right, that a given page should be displayed on
+        // Return which side, left or right, a given page should be displayed on
         getPageSide: function(index) {
             if (0 === (index & 0x1)) {
                 return 'R';
@@ -81,9 +83,6 @@ img.onload = function(event) {
             return spreadIndices;
         },
 
-        // For a given "accessible page index" return the page number in the book.
-        // For example, index 5 might correspond to "Page 1" if there is front matter such
-        // as a title page and table of contents.
         getPageNum: function(index) {
             return index + 1;
         },
@@ -106,4 +105,5 @@ img.onload = function(event) {
     // override an entry in the initUIStrings tooltips method
     $('#BookReader').find('.logo').attr('title', 'Go to VAULT');
 }
-img.src = pageOne; // triggers the image onload handler
+ // load first page, triggers the image onload handler
+img.src = vaultItem.root + 'file/' + vaultItem.id + '/' + vaultItem.version + '/' + vaultItem.filenames + '1.JPG'
